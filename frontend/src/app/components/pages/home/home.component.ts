b1363cdfe862d98d4fb8a6c72a7838c760d7e4fb
Kanelloss <kanellosalmpanis@hotmail.com>
2024-11-10 19:15:05 +0200

    abc

---
+++ frontend/src/app/components/pages/home/home.component.ts
@@ -0,0 +1,19 @@
+// import { Component } from '@angular/core';
+// import { Food } from '../../../shared/models/Food';
+// import { FoodService } from '../../../services/food.service';
+
+// @Component({
+//   selector: 'app-home',
+//   standalone: true,
+//   imports: [],
+//   templateUrl: './home.component.html',
+//   styleUrl: './home.component.css'
+// })
+// export class HomeComponent {
+
+//   food:Food[] = [];
+//   constructor( private foodService:FoodService) {
+//     this.food = foodService
+//   }
+
+// }
